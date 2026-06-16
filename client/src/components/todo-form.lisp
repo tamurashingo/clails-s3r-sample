4d61bc20eefc744426a584867bfebc053ae55209
tamura shingo <tamura.shingo@gmail.com>
2026-06-16 21:59:38 +0000

    use cl-s3r :guard for server-side auth on protected routes (cl-s3r#11)

--- client/src/components/todo-form.lisp
+++ client/src/components/todo-form.lisp
@@ -2,28 +2,24 @@
 (in-package #:cl-user)
 (defpackage #:todo-client/components/todo-form
   (:use #:cl)
-  (:import-from #:cl-s3r.component #:define-component)
-  (:import-from #:cl-s3r.cookie #:get-cookie))
+  (:import-from #:cl-s3r.component #:define-component))
 
 (in-package #:todo-client/components/todo-form)
 
 (define-component todo-form-page (&key error &allow-other-keys)
-  (let ((token (get-cookie "todo-session")))
-    (if (null token)
-        `(:script "window.location.replace('/login');")
-        `(:div
-          (:div (@ (:class "header"))
-            (:h1 "New TODO")
-            (:a (@ (:href "/todos")) "<- Back to list"))
-          ,@(when error `((:p (@ (:class "error")) ,error)))
-          (:form (@ (:action "/todos") (:method "post"))
-            (:label "Title *")
-            (:input (@ (:type "text") (:name "subject") (:required "required") (:autofocus "autofocus")))
-            (:label "Content")
-            (:textarea (@ (:name "content") (:rows "5")) "")
-            (:label "Deadline")
-            (:input (@ (:type "date") (:name "deadline")))
-            (:div
-             (:button (@ (:type "submit")) "Create")
-             " "
-             (:a (@ (:href "/todos")) "Cancel")))))))
+  `(:div
+    (:div (@ (:class "header"))
+      (:h1 "New TODO")
+      (:a (@ (:href "/todos")) "<- Back to list"))
+    ,@(when error `((:p (@ (:class "error")) ,error)))
+    (:form (@ (:action "/todos") (:method "post"))
+      (:label "Title *")
+      (:input (@ (:type "text") (:name "subject") (:required "required") (:autofocus "autofocus")))
+      (:label "Content")
+      (:textarea (@ (:name "content") (:rows "5")) "")
+      (:label "Deadline")
+      (:input (@ (:type "date") (:name "deadline")))
+      (:div
+       (:button (@ (:type "submit")) "Create")
+       " "
+       (:a (@ (:href "/todos")) "Cancel")))))
