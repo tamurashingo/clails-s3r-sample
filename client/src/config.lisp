09861496a274bf507f466a1b75eabe74aa6643a0
tamura shingo <tamura.shingo@gmail.com>
2026-06-17 02:25:48 +0000

    use cl-s3r.config:getenv for environment variable access (cl-s3r#13)

--- client/src/config.lisp
+++ client/src/config.lisp
@@ -2,9 +2,11 @@
 (in-package #:cl-user)
 (defpackage #:todo-client/config
   (:use #:cl)
+  (:import-from #:cl-s3r.config
+                #:getenv)
   (:export #:api-server-url))
 
 (in-package #:todo-client/config)
 
 (defun api-server-url ()
-  (or (uiop:getenv "API_SERVER_URL") "http://localhost:5000"))
+  (getenv "API_SERVER_URL" :default "http://localhost:5000"))
